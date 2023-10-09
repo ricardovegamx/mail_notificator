505833b8be65e0a2dd3f92721057c89593c0c504
Rick Vega <dev.ricardo.vega@gmail.com>
2023-10-09 11:57:15 -0600

    initial version of the email loading with variables

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -1,7 +1,24 @@
+import json
 import logging
+
+from jinja2 import Environment, FileSystemLoader
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def handler(event, context):
-    logging.info(f"event received: {f}")
+
+def lambda_handler(event, context):
+    logging.info(f"event received: {event}")
+
+    message_body = event["Records"][0]["body"]
+    message_data = json.loads(message_body)
+
+    env = Environment(loader=FileSystemLoader("."))
+    template = env.get_template("email_template.html")
+
+    html_content = template.render(message_data)
+    print(html_content)
+
+    # send the email
+
+    # if sent email, delete the message from the queue
